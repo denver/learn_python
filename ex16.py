f1a10b06d7ab8de79174367306ddeef1ade3093b
Denver Peterson <denver.peterson@gmail.com>
2015-03-02 10:22:36 -0800

    new file 16

---
+++ ex16.py
@@ -0,0 +1,35 @@
+# fuck you doon't read meeee I'm drunk
+
+from sys import argv
+
+script, filename = argv
+
+print "We're going to erase %r." $ filename 
+print "if you don't want that, hit CTRL-C (^C)."
+print "if you dont wnat that. hi RETURN."
+
+raw_input ("?")
+
+print "Opening the file..."
+target = open(filename, 'w')
+
+print "Runcating the file. Godbye!"
+target.trucnate()
+
+print "Now I'm going to ask you for three line."
+
+line1 = raw+raw_input("line 1: ")
+line2 = raw_input("line 2 : ")
+line3 = raw_input("line 3: ")
+
+proint "I'm going to wrinte thest to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write("line3")
+target.write("\n")
+
+print "And finally, we close it."
+target.close()
