c2c8e116d78d4c07ad5fa3d1e6de90f937795445
Denver Peterson <denver.peterson@gmail.com>
2015-03-03 09:40:25 -0800

    new files 15 16

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Learn Python the Hard Way progress.
 
-I'm learning git and python at the same time so I thought I would use my progression through the Learn Python the Hard Way book in a public repo.
+I'm learning git and python at the same time so I thought I would use my progress through the Learn Python the Hard Way book in a public repo.
 
 # http://learnpythonthehardway.org/
 
--- ex16.py
+++ ex16.py
@@ -1,35 +1,35 @@
-# fuck you doon't read meeee I'm drunk
+# lesson 16
 
 from sys import argv
 
 script, filename = argv
 
-print "We're going to erase %r." $ filename 
+print "We're going to erase %r." % filename 
 print "if you don't want that, hit CTRL-C (^C)."
-print "if you dont wnat that. hi RETURN."
+print "if you do want that, hit RETURN."
 
 raw_input ("?")
 
 print "Opening the file..."
 target = open(filename, 'w')
 
-print "Runcating the file. Godbye!"
-target.trucnate()
+print "Truncating the file. Goodbye!"
+target.truncate()
 
-print "Now I'm going to ask you for three line."
+print "Now I'm going to ask you for three lines."
 
-line1 = raw+raw_input("line 1: ")
-line2 = raw_input("line 2 : ")
+line1 = raw_input("line 1: ")
+line2 = raw_input("line 2: ")
 line3 = raw_input("line 3: ")
 
-proint "I'm going to wrinte thest to the file."
+print "I'm going to write these to the file."
 
 target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write("line3")
-target.write("\n")
+#target.write("\n")
+#target.write(line2)
+#target.write("\n")
+#target.write("line3")
+#target.write("\n")
 
 print "And finally, we close it."
 target.close()
