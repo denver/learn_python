f1fc527f5b529f0650a7da08331a5a598912c79d
Denver Peterson <denver.peterson@gmail.com>
2015-06-24 00:45:06 +0700

    removed the deleted files hopefully

--- ex2.py
+++
@@ -1,6 +0,0 @@
-# v.1 by Denver Peterson
-print "I could have code like this."  # and this and this
-
-# blah blah blah
-
-print "this will run."
--- my_math.py
+++
@@ -1,8 +0,0 @@
-# this is what I want to calculate
-
-print "This is cool", 5 + 2 - 2.5 + 999.5550000000000
-print 5.25 + 5 
-
-print 5 % 5
-print 100 % 5
-print 7 % 2
