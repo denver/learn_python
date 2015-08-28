f4f9b90857025109863993fa6744710552639c00
Denver Peterson <denver.peterson@gmail.com>
2015-08-28 08:48:55 -0700

    Adding 39 test

---
+++ lessons/ex39_test.py
@@ -0,0 +1,14 @@
+#ex39_test.py
+
+import hashmap
+
+# create a mapping of state to abbreviation
+states = hashmap.new()
+hashmap.set(states, 'Oregon', 'OR')
+hashmap.set(states, 'Florida', 'FL')
+hashmap.set(states, 'California', 'CA')
+hashmap.set(states, 'New York', 'NY')
+hashmap.set(states, 'Michigan', 'MI')
+
+# create a basic set of states and some cities in them
+cities = hashmap.new()
