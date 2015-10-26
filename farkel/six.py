870efc3d0dd3b97b98fc6d5a3b73e8ac9a593e63
Denver Peterson <denver.peterson@gmail.com>
2015-10-25 23:41:12 -0700

    last update of the night - things are broken

--- farkel/six.py
+++ farkel/six.py
@@ -5,23 +5,21 @@ import random
 from urllib import urlopen
 import sys
 
-i = 0
+i = False
 total_rolls = 0
-roll = [random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6)]
+# roll = [random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6)]
 diceleft = 6
 
-print "You rolled %s" % (roll)
+# print "You rolled %s" % (roll)
 
 #for i in diceleft:
 #	print "you rolled > %s" % (roll)
 
-# while i is False:
-# 	roll = [random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6)]
-#     print "You rolled %s" % (roll)
-#     total_rolls = total_rolls + 1
-#     if roll.[0] == roll.[1]:
-#     	print "wow, we made it"
-#     	i = True
+while i is False:
+	roll = [random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6)]
+    print "You rolled %s" % (roll)
+    total_rolls = total_rolls + 1
+    i = True
 
     
     
