eee88c1cd8a786982fda1ac7d095266b3ceb4ee7
Denver Peterson <denver.peterson@gmail.com>
2015-10-25 19:50:20 -0700

    creating first roll file for farkle

--- roll.py
+++ roll.py
@@ -1,5 +1,6 @@
 # random.py
 # generate random integers for test purposes
+
 import random 
 from urllib import urlopen
 import sys
@@ -7,3 +8,4 @@ import sys
 roll = random.randint(1, 6)
 
 print "You rolled %s" % (roll)
+
