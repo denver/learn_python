bd6640f44839be93eb5f71f4be402c77288f2442
Denver Peterson <denver.peterson@gmail.com>
2015-10-25 22:59:05 -0700

    adding files for basic farkel game

---
+++ farkel/farkel.py
@@ -0,0 +1,10 @@
+# farkel python simulator
+
+import random 
+from urllib import urlopen
+import sys
+
+roll = random.randint(1, 6)
+
+print "You rolled %s" % (roll)
+
---
+++ farkel/six.py
@@ -0,0 +1,15 @@
+#six.py
+# random.py
+# generate random integers for test purposes
+import random 
+from urllib import urlopen
+import sys
+
+total_rolls = 0
+roll = []
+
+def die = random.randint(1, 6):
+
+print "You rolled %s" % (roll)
+
+
