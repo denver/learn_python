80d1b87e9a4b6a123b7585019adf4eda23ad4884
Denver Peterson <denver.peterson@gmail.com>
2015-10-27 23:29:28 -0700

    need help from stew

--- farkel/six.py
+++ farkel/six.py
@@ -8,8 +8,11 @@ import sys
 # set variable to print at the end, maintain count
 #total_rolls = 0
 # set diceleft
-diceleft = 6
+diceleft = 112
 #blah = []
+
+elements = []
+#a = []
 
 # commented out, easy "hack" of creating first array of six rolls 
 # roll = [random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6)]
@@ -20,20 +23,29 @@ def checkEqual(lst):
 
 # function to roll remaing dice in cup n = 1-6
 def roll():
-	result = [0]
+	result = []
 	total_rolls = 0
-	a = []
+	#a = []
 	for x in range(0, diceleft):
 		total_rolls = total_rolls + 1
 		roll = random.randint(1, 6)
 		print roll
 		#result = result.append(1)
 		#a = a.append(roll)
-		print a
+		#a = a.append(x)
+		#print a
 		print "Total rolls > %r" % (total_rolls)
 
 # write a test 
 test_var = [ 1, 1, 1, 1, 1, 1,]
 print "Did your test pass? > %r" % (checkEqual(test_var))
+
+
+# then use the range function to do 0 to 5 counts
+# for i in range(0, 6):
+#     print "Adding %d to the list." % i
+#     # append is a function that lists understand
+#     elements.append(i)
+
 
 roll()
